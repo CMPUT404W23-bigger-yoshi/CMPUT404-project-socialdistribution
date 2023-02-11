ce7fdbc57a053b065a01f853454ee2599921cd75
shehraj123 <shehrajsingh@gmail.com>
2023-02-11 15:12:50 -0700

    Added Post model

--- api/app.py
+++ api/app.py
@@ -3,12 +3,16 @@ from flask import Flask, redirect, url_for
 from api.admin import admin_bp
 from api.user import user_bp
 
+from .models import db
+
 # note: Heroku will run things from the working directory as the root of this repo. Therefore, this path MUST
 # be relative to the root of the repo, NOT to this file. You will likely need to specify the working directory
 # as the root of this repo # when you run this file in your IDE
 app = Flask(__name__, static_folder="../frontend/build", static_url_path="/")
 app.register_blueprint(user_bp, url_prefix="/authors")
 app.register_blueprint(admin_bp, url_prefix="/admin")
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/test.db"
+db.init_app(app)
 
 
 @app.route("/")
@@ -22,4 +26,5 @@ def hello_world():
 
 
 if __name__ == "__main__":
+    db.create_all()
     app.run()
