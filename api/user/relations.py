84f469c103dafdc99efec40215cf0f38bbefb882
shehraj123 <shehrajsingh@gmail.com>
2023-02-12 00:15:19 -0700

    Changed naming for relation tables

--- api/user/relations.py
+++ api/user/relations.py
@@ -1,11 +1,11 @@
 from api.app import db
 
-author_comments = db.Table(
+author_likes_comments = db.Table(
     db.Column("auth_id", db.Integer, db.ForeignKey("author.id")),
     db.Column("comment_id", db.Integer, db.ForeignKey("comment.id")),
 )
 
-author_posts = db.Table(
+author_likes_posts = db.Table(
     db.Column("auth_id", db.Integer, db.ForeignKey("author.id")),
     db.Column("post_id", db.Integer, db.ForeignKey("post.id")),
 )
