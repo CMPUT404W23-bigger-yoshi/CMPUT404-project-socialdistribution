35a490f10870578fc4dd8781b22f9ca27c010905
MHUalberta <mohammadhammad50@gmail.com>
2023-02-13 12:13:02 -0700

    Add searchbar

--- frontend/src/components/Sidebar/Sidebar.jsx
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -5,6 +5,15 @@ import { Gear, Bell, Person, Lock, HouseDoor } from 'react-bootstrap-icons';
 import React from 'react';
 import YoshiPhone from '../../static/Yoshi-phone.png';
 
+function Searchbar() {
+  return (
+<div className="input-group">
+  <input type="search" className="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
+  <button type="button" className="btn btn-outline-primary">search</button>
+</div>
+  )
+}
+
 function Sidebar() {
   const location = useLocation(); // Need this for highlighting current location
 
@@ -14,6 +23,7 @@ function Sidebar() {
         <img src={YoshiPhone} alt="BiggerYoshiLogo" className="sidelogo" />
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Searchbar/>
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav activeKey={location.pathname} className="flex-column">
           <Nav.Link href="/login" className="nav-link">
