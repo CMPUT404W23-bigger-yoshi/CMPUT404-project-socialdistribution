11c98b715668a761f521412b54f5d045b464fe9a
MHUalberta <mohammadhammad50@gmail.com>
2023-02-14 21:19:24 -0700

    Remove search bar

--- frontend/src/components/Sidebar/Sidebar.jsx
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -5,23 +5,6 @@ import { Gear, Bell, Person, Lock, HouseDoor } from 'react-bootstrap-icons';
 import React, { useEffect, useState } from 'react';
 import YoshiPhone from '../../static/Yoshi-phone.png';
 
-function Searchbar() {
-  return (
-    <div className='input-group'>
-      <input
-        type='search'
-        className='form-control rounded'
-        placeholder='Search'
-        aria-label='Search'
-        aria-describedby='search-addon'
-      />
-      <button type='button' className='btn btn-outline-primary'>
-        search
-      </button>
-    </div>
-  );
-}
-
 function Sidebar() {
   const location = useLocation(); // Need this for highlighting current location
   const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth <= 768);
@@ -45,7 +28,6 @@ function Sidebar() {
       <Navbar.Brand href='/'>
         <img src={YoshiPhone} alt='BiggerYoshiLogo' className='sidelogo' />
       </Navbar.Brand>
-      {!isSmallScreen && <Searchbar />}
       <Navbar.Collapse id='basic-navbar-nav'>
         <Nav activeKey={location.pathname} className='flex-column'>
           <Nav.Link href='/login' className='nav-link'>
