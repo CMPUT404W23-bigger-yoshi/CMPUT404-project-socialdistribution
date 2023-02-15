1429c2632581651e9a1dc23efb54b7db26fd6336
Mattwmaster58 <mattwmaster58@gmail.com>
2023-02-14 21:56:59 -0700

    fix: recontextualize comments

--- api/app.py
+++ api/app.py
@@ -7,16 +7,12 @@ from api.user.author import model
 from api.user.comments import model
 from api.user.posts import model
 
-# db must be initialized before importing models
-
-
-# note: Heroku will run things from the working directory as the root of this repo. Therefore, this path MUST
-# be relative to the root of the repo, NOT to this file. You will likely need to specify the working directory
-# as the root of this repo # when you run this file in your IDE
-
 
 def create_app():
     app = Flask(__name__, static_folder="../frontend/build", static_url_path="/")
+    # note: Heroku will run things from the working directory as the root of this repo. Therefore, this path MUST
+    # be relative to the root of the repo, NOT to this file. You will likely need to specify the working directory
+    # as the root of this repo # when you run this file in your IDE
     app.register_blueprint(user_bp, url_prefix="/authors")
     app.register_blueprint(admin_bp, url_prefix="/admin")
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///bigger_yoshi.db"
