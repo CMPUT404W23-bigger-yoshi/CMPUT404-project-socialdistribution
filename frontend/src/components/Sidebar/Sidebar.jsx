ef4bcca63780a56136efc19eadf40a9268ffe31b
MHUalberta <mohammadhammad50@gmail.com>
2023-02-14 20:47:30 -0700

    Progress for sidebar2navbar

--- frontend/src/components/Sidebar/Sidebar.jsx
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -2,50 +2,74 @@ import './Sidebar.css';
 import { Nav, Navbar } from 'react-bootstrap';
 import { useLocation } from 'react-router-dom';
 import { Gear, Bell, Person, Lock, HouseDoor } from 'react-bootstrap-icons';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import YoshiPhone from '../../static/Yoshi-phone.png';
 
 function Searchbar() {
   return (
-<div className="input-group">
-  <input type="search" className="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
-  <button type="button" className="btn btn-outline-primary">search</button>
-</div>
-  )
+    <div className='input-group'>
+      <input
+        type='search'
+        className='form-control rounded'
+        placeholder='Search'
+        aria-label='Search'
+        aria-describedby='search-addon'
+      />
+      <button type='button' className='btn btn-outline-primary'>
+        search
+      </button>
+    </div>
+  );
 }
 
 function Sidebar() {
   const location = useLocation(); // Need this for highlighting current location
+  const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth <= 768);
+
+  useEffect(() => {
+    function handleResize() {
+      setIsSmallScreen(window.innerWidth <= 768);
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const smallScreenClass = isSmallScreen ? 'sidebar-top' : '';
+  const navbarClassList = `sidebar ${smallScreenClass}`;
 
   return (
-    <Navbar expand="md" className="sidebar">
-      <Navbar.Brand href="/">
-        <img src={YoshiPhone} alt="BiggerYoshiLogo" className="sidelogo" />
+    <Navbar className={navbarClassList}>
+      <Navbar.Brand href='/'>
+        <img src={YoshiPhone} alt='BiggerYoshiLogo' className='sidelogo' />
       </Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Searchbar/>
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav activeKey={location.pathname} className="flex-column">
-          <Nav.Link href="/login" className="nav-link">
-            <HouseDoor /> Home
+      {!isSmallScreen && <Searchbar />}
+      <Navbar.Collapse id='basic-navbar-nav'>
+        <Nav activeKey={location.pathname} className='flex-column'>
+          <Nav.Link href='/login' className='nav-link'>
+            <HouseDoor /> {!isSmallScreen && 'Home'}
           </Nav.Link>
-          <Nav.Link href="/login">
-            <Person /> Profile
+          <Nav.Link href='/login'>
+            <Person /> {!isSmallScreen && 'Profile'}
           </Nav.Link>
-          <Nav.Link href="/login">
-            <Lock /> Private Posts
+          <Nav.Link href='/login'>
+            <Lock /> {!isSmallScreen && 'Private Posts'}
           </Nav.Link>
-          <Nav.Link href="/login">
-            <Bell /> Notifications
+          <Nav.Link href='/login'>
+            <Bell /> {!isSmallScreen && 'Notifications'}
           </Nav.Link>
-          <Nav.Link href="/login">
-            <Gear /> Settings
+          <Nav.Link href='/login'>
+            <Gear /> {!isSmallScreen && 'Settings'}
           </Nav.Link>
         </Nav>
       </Navbar.Collapse>
-      <button type="button" className="admin-button">
-        Admin
-      </button>
+      {!isSmallScreen && (
+        <button type='button' className='admin-button'>
+          Admin
+        </button>
+      )}
     </Navbar>
   );
 }
