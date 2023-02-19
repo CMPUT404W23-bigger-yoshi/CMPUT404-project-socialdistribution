a93e0b6ad636a6825582bacc3fabf9c64f40b2d5
AP <akshatpandeymyself@gmail.com>
2023-02-18 18:16:43 -0700

    ft: Basic layout for profile header

--- frontend/src/components/Profile/Profile.jsx
+++ frontend/src/components/Profile/Profile.jsx
@@ -1,12 +1,90 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Profile.css';
+import { Button, Col, Row } from 'react-bootstrap';
+import { Github, Twitter } from 'react-bootstrap-icons';
 
 const Profile = () => {
+  const [user, setUser] = useState({
+    id: 'https://www.facebook.com/100009000000000',
+    host: 'https://www.facebook.com',
+    displayName: 'Manpreet Kaur',
+    url: 'https://www.facebook.com/100009000000000',
+    github: 'https://github.com/manpreetkaur',
+    twitter: 'https://twitter.com/manpreetkaur',
+    profileImage: 'https://i.imgur.com/uyUFvIp.png'
+  });
+  const [userFollowStats, setUserFollowStats] = useState({
+    following: 56,
+    followers: 45,
+    friends: 2
+  });
+  useEffect(() => {
+    const fetchUser = async () => {
+      const res = await fetch('http://localhost:8080/api/user');
+      const data = await res.json();
+      setUser(data);
+    };
+    fetchUser().then(r => console.log(r));
+  }, []);
+  useEffect(() => {
+    const fetchUserFollowStats = async () => {
+      const res = await fetch('http://localhost:8080/api/user/follow-stats');
+      const data = await res.json();
+      setUserFollowStats(data);
+    };
+    fetchUserFollowStats().then(r => console.log(r));
+  }, []);
   return (
     <div className='profile'>
       <div className='profile-border'>
         <div className='profile-container'>
-          <h1>Profile</h1>
+          <div className='profile-info'>
+            <div className='profile-image'>
+              <img src={user.profileImage}
+                   alt='profile' />
+            </div>
+            <div className='profile-name'>
+              <h1>{user.displayName}</h1>
+            </div>
+            <div className='profile-follow-stats'>
+              <Row className='profile-follow-stats-row' xs={3}>
+                <Col>
+                  <h3>{userFollowStats.following}</h3>
+                  <p>Following</p>
+                </Col>
+                <Col>
+                  <h3>{userFollowStats.friends}</h3>
+                  <p>Friends</p>
+                </Col>
+                <Col>
+                  <h3>{userFollowStats.followers}</h3>
+                  <p>Followers</p>
+                </Col>
+              </Row>
+            </div>
+            <div className='profile-links'>
+              <div
+                className='profile-link github'
+                onClick={() => window.open(user.github, '_blank')}
+              >
+                <Github />
+              </div>
+              <div
+                className='profile-link twitter'
+                onClick={() => window.open(user.url, '_blank')}
+              >
+                <Twitter />
+              </div>
+            </div>
+          </div>
+          <div className='profile-buttons'>
+            <Button className='profile-button follow'>
+              Follow
+            </Button>
+            <Button className='profile-button share'>
+              Share
+            </Button>
+          </div>
         </div>
       </div>
     </div>
