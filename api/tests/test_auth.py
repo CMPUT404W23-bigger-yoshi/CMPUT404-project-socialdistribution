43658b79eedefd1357f361c4edd2bada866ba86d
shehraj123 <shehrajsingh@gmail.com>
2023-03-28 20:59:13 -0600

    Added pytest coverage and remove warnings from pytest

--- api/tests/test_auth.py
+++ api/tests/test_auth.py
@@ -1,10 +1,11 @@
 import pytest
 
 from api import API_ROOT
-from api.user.author import model
+from api.tests.resources.mock_authors import *
+from api.user.author.model import Author
 
 
-class TestAuth:
+class TestAuthUnit:
 
     """
     Test registering mock user.
@@ -13,14 +14,10 @@ class TestAuth:
     def test_register(self, client, app):
         response = client.post(f"{API_ROOT}/authors/register", json={"username": "test", "password": "test"})
 
-        # TODO Should be a redirect ????
-        print(response.data)
         assert response.status_code == 200
-        # assert response.header["Location"] == "/auth/login"
 
-        # Author should be created in database
         with app.app_context():
-            assert model.Author.query.filter_by(username="test").first()
+            assert Author.query.filter_by(username="test").first()
 
     """
     Test registering with invalid credentials.
@@ -60,11 +57,70 @@ class TestAuth:
         response = client.post(f"{API_ROOT}/authors/logout")
         assert response.status_code == 200
 
-        """
-        Use the underneath if flask has to redirect.
-        Add follow_redirects=True to post call above
-        """
-        # # Check that there was one redirect response.
-        # assert len(response.history) == 1
-        # # Check that the second request was to the ipage.
-        # assert response.request.path == "/login"
+
+"""
+API tests are for matching expected response from server
+"""
+
+
+class TestAuthorAPI:
+
+    """
+    Test get all authors
+    """
+
+    def test_get_all_authors(self, client, auth):
+        cred1 = {"username": mock_author1["displayName"], "password": "123"}
+        cred2 = {"username": mock_author2["displayName"], "password": "123"}
+
+        register = client.post(f"{API_ROOT}/authors/register", json=cred1)
+        assert register.status_code == 200
+
+        register = client.post(f"{API_ROOT}/authors/register", json=cred2)
+        assert register.status_code == 200
+
+        # Register and Login as test to access API without BasicAuth
+        auth.register()
+        auth.login()
+
+        response = client.get(f"{API_ROOT}/authors", follow_redirects=True)
+        assert response.status_code == 200
+        data = response.json
+
+        # Check keys
+        type = data.get("type", None)
+        assert type is not None
+
+        items = data.get("items", None)
+        assert items is not None
+
+        author_list = data.get("items")
+        usernames = set()
+        for author in author_list:
+            assert set(mock_author1.keys()) == set(author.keys())
+            usernames.add(author["displayName"])
+
+        assert mock_author1["displayName"] in usernames and mock_author2["displayName"] in usernames
+
+    """
+    Test get single author
+    """
+
+    def test_get_single_author(self, client, auth, app):
+        auth.register()
+
+        with app.app_context():
+            test_author = Author.query.filter_by(username="test").first()
+        assert test_author
+
+        id = test_author.id
+
+        response = client.get(f"{API_ROOT}/authors/{id}", follow_redirects=True)
+        assert response.status_code == 200
+
+        author = response.json
+        assert set(mock_author1.keys()) == set(author.keys())
+
+        url_id = author["id"]
+        data_id = url_id.split("/")[-1]
+        assert data_id == id
