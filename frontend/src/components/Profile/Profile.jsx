217a66a4f2ecd26080717bbca455ecd41e10778d
AP <akshatpandeymyself@gmail.com>
2023-04-01 09:05:19 -0600

    pre-commit

--- frontend/src/components/Profile/Profile.jsx
+++ frontend/src/components/Profile/Profile.jsx
@@ -5,7 +5,12 @@ import { Github, Twitter } from 'react-bootstrap-icons';
 import ShareModal from '../ShareModal/ShareModal';
 import Post from '../Post/Post';
 import { useLocation, useNavigate } from 'react-router-dom';
-import { getFollowersCount, getUserDetails, sendFollowRequest, unfollowUser } from '../../services/author';
+import {
+  getFollowersCount,
+  getUserDetails,
+  sendFollowRequest,
+  unfollowUser
+} from '../../services/author';
 import { getPosts } from '../../services/post';
 import { AuthorContext } from '../../context/AuthorContext';
 import GitHubCalendar from 'react-github-calendar';
@@ -67,40 +72,40 @@ const Profile = ({ authorUrl }) => {
   }
 
   return (
-    <div className='profile'>
+    <div className="profile">
       <ShareModal
         show={showShareModal}
         handleClose={() => setShowShareModal(false)}
         link={user.id}
       />
-      <div className='profile-border'>
-        <div className='profile-container'>
-          <div className='profile-info'>
-            <div className='profile-image'>
+      <div className="profile-border">
+        <div className="profile-container">
+          <div className="profile-info">
+            <div className="profile-image">
               {user.profileImage ? (
-                <img src={user.profileImage} alt='profile' />
+                <img src={user.profileImage} alt="profile" />
               ) : (
                 <img
-                  src='https://i.pinimg.com/originals/f1/0f/f7/f10ff70a7155e5ab666bcdd1b45b726d.jpg'
-                  alt='profile'
+                  src="https://i.pinimg.com/originals/f1/0f/f7/f10ff70a7155e5ab666bcdd1b45b726d.jpg"
+                  alt="profile"
                 />
               )}
             </div>
-            <div className='profile-name'>
+            <div className="profile-name">
               <h1>{user.displayName}</h1>
             </div>
-            <div className='profile-follow-stats'>
-              <Row className='profile-follow-stats-row' xs={1}>
-                <Col className='px-4'>
+            <div className="profile-follow-stats">
+              <Row className="profile-follow-stats-row" xs={1}>
+                <Col className="px-4">
                   <h3>{userFollowStats.followers}</h3>
                   <p>Followers</p>
                 </Col>
               </Row>
             </div>
-            <div className='profile-links'>
+            <div className="profile-links">
               {user.github && (
                 <div
-                  className='profile-link github'
+                  className="profile-link github"
                   onClick={() => window.open(user.github, '_blank')}
                 >
                   <Github />
@@ -108,7 +113,7 @@ const Profile = ({ authorUrl }) => {
               )}
               {user.twitter && (
                 <div
-                  className='profile-link twitter'
+                  className="profile-link twitter"
                   onClick={() => window.open(user.twitter, '_blank')}
                 >
                   <Twitter />
@@ -116,9 +121,9 @@ const Profile = ({ authorUrl }) => {
               )}
             </div>
           </div>
-          <div className='profile-buttons'>
+          <div className="profile-buttons">
             <Button
-              className='profile-button follow'
+              className="profile-button follow"
               onClick={async () => {
                 if (loggedInAuthor.id === user.id) {
                   navigate('/settings');
@@ -142,11 +147,11 @@ const Profile = ({ authorUrl }) => {
               {loggedInAuthor.id === user.id
                 ? 'Edit'
                 : following
-                  ? 'Unfollow'
-                  : 'Follow'}
+                ? 'Unfollow'
+                : 'Follow'}
             </Button>
             <Button
-              className='profile-button share'
+              className="profile-button share"
               onClick={() => setShowShareModal(true)}
             >
               Share
@@ -154,14 +159,14 @@ const Profile = ({ authorUrl }) => {
           </div>
         </div>
       </div>
-      <div className='profile-post-border'>
-        <div className='profile-post-container'>
+      <div className="profile-post-border">
+        <div className="profile-post-container">
           {user.github && (
-            <div className='profile-github'>
+            <div className="profile-github">
               <GitHubCalendar username={user.github.split('/').pop()} />
             </div>
           )}
-          <div className='profile-posts'>
+          <div className="profile-posts">
             {posts?.items?.length > 0 ? (
               posts.items.map((post) => (
                 <Post
@@ -173,7 +178,7 @@ const Profile = ({ authorUrl }) => {
                 />
               ))
             ) : (
-              <div className='no-posts'>
+              <div className="no-posts">
                 <h1>No posts to show</h1>
               </div>
             )}
@@ -181,7 +186,7 @@ const Profile = ({ authorUrl }) => {
         </div>
       </div>
     </div>
-  )
+  );
 };
 
 export default Profile;
